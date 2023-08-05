a04653851b1691c3d44db84fcaa41a6d58c4d72e
magalercio <magalercio.gui@gmail.com>
2023-08-05 18:36:00 -0300

    Modal + typescript
    Adcionando typescript ao projeto.
    Criado o modal, sem as funcoes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,38 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import React from 'react';
 import './App.css'
 import { Plus } from 'react-feather'
-import styles from './styles/pages/home.modules.css'
+import styles from './styles/pages/home.module.css'
+import { Task } from './components/Task/index';
+import { CreateTaskModal } from './components/CreateTaskModal/index';
+import Modal from "react-modal"
+
+Modal.setAppElement("#root");
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const isCreateTaskModalOpen = false;
 
   return (
     <div className="App">
       <section className={styles.container}>
-        <header className={styles.header}> Minhas tarefas</header>
-        <button className={styles.addTaskButton}>Adicionar uma nova tarefa <Plus/></button>
+        <header className={styles.header}>
+          <h1>Minhas tarefas</h1> 
+        <button className={styles.addTaskButton}>Adicionar uma nova tarefa 
+        <Plus/>
+        </button>
+        </header>
+        <div className={styles.tasks}>
+        <Task />
+        </div>
       </section>
+
+      <CreateTaskModal isOpen={isCreateTaskModalOpen}/>
     </div>
   )
 }
 
 export default App
+
